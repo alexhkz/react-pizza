336989cbe1a90ed109b6409726c83fd0526c0293
alexhkz <alexwebcode@gmail.com>
2021-09-06 17:36:54 +0300

    categories1

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import classNames from 'classnames';
+
+const Button = ({onClick, className, outline, children}) => {
+	return (
+		<div>
+			<button 
+				onClick={onClick}
+				className={classNames('button', className,
+			{
+				'button--outline': outline,
+			})}>
+				{children}
+			</button>
+		</div>
+	);
+};
+
+export default Button;
