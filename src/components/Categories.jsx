09262eb66a0589cf5f065cfb3a272b638a26320d
alexhkz <alexwebcode@gmail.com>
2021-09-09 17:20:29 +0300

    sortPopup1

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -15,7 +15,7 @@ const Categories = ({items, onClickItem}) => {
 					<li 
 						className={activeItem === null ? 'active' : ''}  
 						onClick={() => onSelectItem(null)}>Все</li>
-						{items.map((name, index) => (
+						{items && items.map((name, index) => (
 							<li 
 								className={activeItem === index ? 'active' : ''} 
 								onClick={() => onSelectItem(index)} 
