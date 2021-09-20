4899001d965fd233a60c5b779fdffad7ad4af9d9
alexhkz <alexwebcode@gmail.com>
2021-09-20 16:52:23 +0300

    database

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Route } from 'react-router';
 
 import {Header} from './components';
 import {Home, Cart} from './pages';
+
 function App() {
+	const [pizzas, setPizzas] = useState([]);
+
+	useEffect(() => {
+		fetch('http://localhost:3000/db.json')
+			.then((response) => response.json())
+			.then(json => setPizzas(json.pizzas))
+	}, []);
 
   return (
 		<div className="wrapper">
 			<Header />
 		<div className="content">
-			<Route path='/' component={Home} exact/>
+			<Route path='/' render={() => <Home items={pizzas} />} exact/>
 			<Route path='/cart' component={Cart} exact/>
 		</div>
 	</div>
