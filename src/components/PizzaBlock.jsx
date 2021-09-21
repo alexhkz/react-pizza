4f86a09bf92418cd0ef952fc4b437707526f579d
alexhkz <alexwebcode@gmail.com>
2021-09-21 16:11:31 +0300

    typesPizzas

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,28 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
 
-const PizzaBlock = ({name}) => {
+const PizzaBlock = ({name, imageUrl, price, types}) => {
+
+	const typeNames = ['тонкое', 'традиционное'];
+	const [activeType, setActiveType] = useState(0);
+
+	const onSelectType = (index) => {
+		setActiveType(index);
+	}
+
 	return (
 		<div>
 			<div className="pizza-block">
 				<img
 				className="pizza-block__image"
-				src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
+				src={imageUrl}
 				alt="Pizza"
 				/>
 				<h4 className="pizza-block__title">{name}</h4>
 				<div className="pizza-block__selector">
 				<ul>
-					<li className="active">тонкое</li>
-					<li>традиционное</li>
+					{typeNames.map((type, index) => (
+						<li 
+							key={type} 
+							onClick={() => {onSelectType(index)}} 
+							className={classNames({
+								'active': activeType === index,
+								'disabled': !types.includes(index)
+							})}>{type}
+						</li>
+					))}
 				</ul>
 				<ul>
 					<li className="active">26 см.</li>
-					<li>30 см.</li>
+					<li className="disabled">30 см.</li>
 					<li>40 см.</li>
 				</ul>
 				</div>
 				<div className="pizza-block__bottom">
-				<div className="pizza-block__price">от 395 ₽</div>
+				<div className="pizza-block__price">от {price} ₽</div>
 				<div className="button button--outline button--add">
 					<svg
 					width="12"
