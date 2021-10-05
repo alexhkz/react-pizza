25729a290cbe300a46526e7ab72d6ffebb919ae7
alexhkz <alexwebcode@gmail.com>
2021-10-05 15:54:38 +0300

    add redux

--- src/components/SortPopup.jsx
+++ src/components/SortPopup.jsx
@@ -4,8 +4,7 @@ const SortPopup = ({items}) => {
 	const [visiblePopup, setVisiblePopup] = useState(false);
 	const [activeItem, setActiveItem] = useState(0);
 	const sortRef = useRef();
-
-	const activeLabel = items[activeItem];
+	const activeLabel = items[activeItem].name;
 
 	const toggleVisiblePopup = () => {
 		setVisiblePopup(!visiblePopup);
@@ -50,11 +49,11 @@ const SortPopup = ({items}) => {
 				</div>
 				{visiblePopup && <div className="sort__popup">
 					<ul>
-						{items && items.map((name, index) => (
+						{items && items.map((obj, index) => (
 							<li 
 								onClick={() => onSelectItem(index)} 
 								className={activeItem === index ? 'active' : ''} 
-								key={`${name}_${index}`}>{name}
+								key={`${obj.type}_${index}`}>{obj.name}
 							</li>
 						))}
 					</ul>
