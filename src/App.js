626597b5612226c0e9fd4e20ab732b0e7f9ada9c
alexhkz <alexwebcode@gmail.com>
2021-10-08 16:22:00 +0300

    class component1

--- src/App.js
+++ src/App.js
@@ -1,29 +1,56 @@
 import React, {useEffect, useState} from 'react';
 import { Route } from 'react-router';
 import axios from 'axios';
+import { connect } from 'react-redux';
 
 import {Header} from './components';
 import {Home, Cart} from './pages';
+import { setPizzas } from './redux/actions/pizzas';
+import store from './redux/store';
 
-function App() {
-	const [pizzas, setPizzas] = useState([]);
+// function App() {
 
-	useEffect(() => {
+// 	useEffect(() => {
+// 		axios.get('http://localhost:3000/db.json')
+// 		.then(({data}) => {
+// 			setPizzas(data.pizzas)
+// 		});
+// 	}, []);
+
+//   return ;
+// }
+
+class App extends React.Component {
+	componentDidMount() {
 		axios.get('http://localhost:3000/db.json')
 		.then(({data}) => {
-			setPizzas(data.pizzas)
+ 			this.props.setPizzas(data.pizzas);
 		});
-	}, []);
+	}
 
-  return (
-		<div className="wrapper">
-			<Header />
-		<div className="content">
-			<Route path='/' render={() => <Home items={pizzas} />} exact/>
-			<Route path='/cart' component={Cart} exact/>
+	render() {
+		console.log(this.props)
+		return (
+			<div className="wrapper">
+				<Header />
+			<div className="content">
+				<Route path='/' render={() => <Home items={this.props.items} />} exact/>
+				<Route path='/cart' component={Cart} exact/>
+			</div>
 		</div>
-	</div>
-  );
+	  )
+	}
 }
 
-export default App;
+const mapStateToProps = state => {
+	return {
+		items: state.pizzas.items,
+		filters: state.filters
+	}
+};
+
+const mapDispatchToProps = {
+	setPizzas
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,8 @@
 import { createStore } from "redux";
+import rootReducer from "./reducers";
 
-function counter(state = 0, action) {
-	switch (action.type) {
-	  case 'INCREMENT':
-		 return state + 1
-	  case 'DECREMENT':
-		 return state - 1
-	  default:
-		 return state
-	}
- }
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-const store = createStore(counter, 5);
+window.store = store;
 
 export default store;
